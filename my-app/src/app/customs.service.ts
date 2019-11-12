e0488858a857e352441aeb612e5e7364a1a72ea4
giorgio1985 <g.adonoo@yahoo.it>
2019-11-12 23:48:07 +0100

    another commit

--- my-app/src/app/customs.service.ts
+++ my-app/src/app/customs.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Customs } from './customs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomsService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private customs: Customs) { }
+
+  anagrafica(customs) {
+  //this.http.post() 
+  }
+
 }
